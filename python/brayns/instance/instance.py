987f6bc25e2701630768f1cefa72a22a373c1655
Adrien4193 <adrien.fleury93@gmail.com>
2022-03-18 16:27:27 +0100

    Add instance class.

---
+++ python/brayns/instance/instance.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2015-2022 EPFL/Blue Brain Project
+# All rights reserved. Do not distribute without permission.
+#
+# Responsible Author: adrien.fleury@epfl.ch
+#
+# This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3.0 as published
+# by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import logging
+from typing import Optional
+
+from ..client.client import Client
+
+
+class Instance:
+
+    def __init__(
+        self,
+        uri: str,
+        secure: bool = False,
+        cafile: Optional[str] = None,
+        loglevel=logging.ERROR
+    ) -> None:
+        self._client = Client(
+            uri=uri,
+            secure=secure,
+            cafile=cafile,
+            loglevel=loglevel
+        )
