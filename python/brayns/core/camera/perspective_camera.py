1d740f56f6cbfdcc650d25b354db8a39543f395a
Adrien4193 <adrien.fleury93@gmail.com>
2022-04-20 10:54:06 +0200

    New camera API.

--- python/brayns/core/camera/perspective_camera.py
+++ python/brayns/core/camera/perspective_camera.py
@@ -21,18 +21,38 @@
 import math
 from dataclasses import InitVar, dataclass
 
+from brayns.core.camera.camera import Camera
 from brayns.core.camera.camera_view import CameraView
 from brayns.core.geometry.axis import Axis
 from brayns.core.geometry.box import Box
+from brayns.instance.instance_protocol import InstanceProtocol
 
 
 @dataclass
-class PerspectiveCamera:
+class PerspectiveCamera(Camera):
 
     fovy: InitVar[float] = math.radians(45)
     aperture_radius: float = 0.0
     focus_distance: float = 1.0
     degrees: InitVar[bool] = False
+
+    @classmethod
+    def get_name(cls) -> str:
+        return 'perspective'
+
+    @classmethod
+    def from_main_camera(cls, instance: InstanceProtocol) -> 'PerspectiveCamera':
+        result = instance.request('get-camera-perspective')
+        return PerspectiveCamera.deserialize(result)
+
+    @classmethod
+    def deserialize(cls, message) -> 'PerspectiveCamera':
+        return PerspectiveCamera(
+            fovy=message['fovy'],
+            aperture_radius=message['aperture_radius'],
+            focus_distance=message['focus_distance'],
+            degrees=True
+        )
 
     def __post_init__(self, fovy: float, degrees: bool) -> None:
         self._fovy = math.radians(fovy) if degrees else fovy
@@ -53,12 +73,22 @@ class PerspectiveCamera:
     def fovy_degrees(self, value: float) -> None:
         self._fovy = math.radians(value)
 
-    def get_full_screen_distance(self, target_height: float) -> float:
-        return target_height / 2 / math.tan(self.fovy_radians / 2)
+    def use_as_main_camera(self, instance: InstanceProtocol) -> None:
+        instance.request('set-camera-perspective', self.serialize())
+
+    def serialize(self) -> dict:
+        return {
+            'fovy': self.fovy_degrees,
+            'aperture_radius': self.aperture_radius,
+            'focus_distance': self.focus_distance
+        }
+
+    def center(self, instance: InstanceProtocol, target: Box) -> None:
+        view = self.get_full_screen_view(target)
+        view.use_for_main_camera(instance)
 
     def get_full_screen_view(self, target: Box) -> CameraView:
         center = target.center
-        height = target.height
-        distance = self.get_full_screen_distance(height)
+        distance = target.height / 2 / math.tan(self.fovy_radians / 2)
         position = center + distance * Axis.forward
         return CameraView(position, center)
