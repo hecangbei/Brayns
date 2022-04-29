a422275d6efa1230136bcd6cb7598c601af40a5d
Adrien4193 <adrien.fleury93@gmail.com>
2022-04-29 11:16:46 +0200

    Report optional in BBPLoader.

--- python/brayns/plugins/bbp/bbp_loader.py
+++ python/brayns/plugins/bbp/bbp_loader.py
@@ -60,10 +60,8 @@ class BbpLoader:
         return loader.add_model(instance, path)
 
     def _get_properties(self) -> dict:
-        report = BbpReport.none() if self.report is None else self.report
         params = {
             'percentage': self.cells.density,
-            'report_type': report.type,
             'neuron_morphology_parameters': {
                 'radius_multiplier': self.radius_multiplier,
                 'load_soma': self.load_soma,
@@ -73,12 +71,14 @@ class BbpLoader:
             'load_afferent_synapses': self.load_afferent_synapses,
             'load_efferent_synapses': self.load_efferent_synapses
         }
-        if report.spike_transition_time is not None:
-            params['spike_transition_time'] = report.spike_transition_time
-        if report.name is not None:
-            params['report_name'] = report.name
         if self.cells.gids is not None:
             params['gids'] = self.cells.gids
         if self.cells.targets is not None:
             params['targets'] = self.cells.targets
+        if self.report is not None:
+            params['report_type'] = self.report.type
+            if self.report.name is not None:
+                params['report_name'] = self.report.name
+            if self.report.spike_transition_time is not None:
+                params['spike_transition_time'] = self.report.spike_transition_time
         return params
